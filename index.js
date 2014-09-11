5ab7fde48f751a6acbec2346d1d7153a613cf6c4
Roman <s91630277@gmail.com>
2014-09-11 19:55:06 +0400

    traceur compile api changed

--- index.js
+++ index.js
@@ -7,17 +7,24 @@ module.exports = function(file) {
 	var result = traceur.compile(contents, {
   		filename: file,
   		modules: "commonjs",
+  		// sourceMaps: true,
   		experimental: true
 	});
 	if (result.error) throw result.error;
+
+	var code = result.js || result;
+	if (typeof code != "string") {
+		throw "Result is not a string.";
+	}
+
 	var newname = "__" + file + ".ntr.js";
-	if (result.js.indexOf("$traceurRuntime") != -1) {
+	if (code.indexOf("$traceurRuntime") != -1) {
 		var runtimePath = traceur.RUNTIME_PATH
 			// .replace(/ /g, "\\ ")
 			.replace(/\\/g, "/");
 		var requireRuntime = format("require(\"%s\");", runtimePath);
-		result.js = [requireRuntime, result.js].join("\n");
+		code = [requireRuntime, code].join("\n");
 	}
-	fs.writeFileSync(newname, result.js);
+	fs.writeFileSync(newname, code);
 	return newname;
 }
